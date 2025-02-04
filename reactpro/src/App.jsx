d2d154c4ffbea7c8c6e2545e9048852f0e939712
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-03 22:57:27 -0600

    Update Fruits.jsx -rendering array of  objects

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,15 +1,13 @@
 // Import JSX files to use
-import Hello from "./components/Hello";
+import Hello from './components/Hello';
 import Fruits from './components/Fruits';
 // Rendering Arrays Or Lists In React
 function App() {
-  
-   return (
-    <div className="App">
-      <Fruits />
-    </div>
-  );
-};
+    return (
+        <div className="App">
+            <Fruits />
+        </div>
+    );
+}
 
 export default App;
-
