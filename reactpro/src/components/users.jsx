ac6d6c50e9df663646802e8d3b9ec503cbc7a474
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-06 22:26:38 -0600

    Update Users.jsx

--- reactpro/src/components/users.jsx
+++ reactpro/src/components/users.jsx
@@ -1,20 +1,17 @@
 export default function Users() {
     //const fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Pineapple'];
     const Users = [
-        { name: 'Apple', price: 10, emoji: '🍎' },
-        { name: 'Mango', price: 7, emoji: '🥭' },
-        { name: 'Banana', price: 2, emoji: '🍌' },
-        { name: 'Orange', price: 5, emoji: '🍊' },
-        { name: 'Pineapple', price: 8, emoji: '🍍' },
+        { name: 'Jack' },
+        { name: 'Sam' },
+        { name: 'Jane' },
+        { name: 'Mark' },
+        { name: 'Mae' },
     ];
     return (
         <div>
             <ul>
-                {fruits.map((fruit) => (
-                    <li key={fruit}>
-                        {' '}
-                        {fruit.emoji} {fruit.name} ${fruit.price}
-                    </li>
+                {users.map((users) => (
+                    <li key={users}> {fruit.name}</li>
                 ))}
             </ul>
         </div>
