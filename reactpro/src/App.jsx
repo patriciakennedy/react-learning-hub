97d3e6f23ca4bd45e88a10449a241ed84fee7b81
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-02-13 22:28:18 -0600

    Modified App.jsx to include new component

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,15 +1,5 @@
-// Import JSX files to use
-import Hello from './components/Hello';
-import Fruits from './components/Fruits';
-import Courses from './components/Courses';
-// Rendering Arrays Or Lists In React
-function App() {
-    const seatNumbers = [1, 4, 7];
-    return (
-        <div className="App">
-            <Hello name="Rob" message="Hi there" seatNumbers={seatNumbers} />
-        </div>
-    );
-}
+const App = () => {
+    return <section>hello</section>;
+};
 
 export default App;
